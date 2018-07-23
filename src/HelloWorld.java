927a635f495ce8d831fe77850453269b186742f7
zaneagley <zaneagley@gmail.com>
2018-07-23 16:43:25 -0500

    added 2nd exercise for java.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,22 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-    }
+
+        int myFavoriteNumber = 25;
+        char myString = 'm';
+        float myNumber = 3.14f;
+
+        System.out.print(myNumber);
+
+        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) "three";
+        }
 }
