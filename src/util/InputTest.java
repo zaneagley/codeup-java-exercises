c739db8013b97fc5a5e93ddffa1845dbb08eb4a2
zaneagley <zaneagley@gmail.com>
2018-07-30 16:13:31 -0500

    added exercise files for java object oriented programming lessons.

---
+++ src/util/InputTest.java
@@ -0,0 +1,14 @@
+package util;
+
+public class InputTest {
+    static Input sc = new Input();
+
+    public static void main(String[] args) {
+
+     //   System.out.println(sc.getDouble(10, 50, "String"));
+     //   System.out.println(sc.getInt(10,40, "String2"));
+     //   System.out.println(sc.getString("String3"));
+     //   System.out.println(sc.yesNo("String4"));
+
+    }
+}
